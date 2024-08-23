e976977bde6a5b713dd2811d6c4d0b1d34c6e986
Nouman Hafeez <noumanhafeez095@gmail.com>
2024-08-23 16:06:17 +0500

    modify pipelines

--- ecommerce_web/ecommerce_web/pipelines.py
+++ ecommerce_web/ecommerce_web/pipelines.py
@@ -13,25 +13,6 @@ class EcommerceWebPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
-        convert_to_floats = ['price', 'available_stock', 'original_price']
-        for i in convert_to_floats:
-            value = adapter.get(i)
-            if value is not None:
-                # Remove any currency symbols and commas, then strip any leading/trailing whitespace
-                value = value.replace('SAR', '').replace(',', '').strip()
-                adapter[i] = float(value)
-
-        model_texts = adapter.get('model_number')
-        model_text = model_texts[2]
-        model_number = model_text.split(':')[-1].strip()
-        adapter['model_number'] = model_number
-
-        img_style = adapter.get('image')
-        start_index = img_style.find("url(") + 4  # Find the start index of the URL within the 'url()' string
-        end_index = img_style.find(")", start_index)  # Find the end index of the URL
-        image_url = img_style[start_index:end_index]  # Extract the URL substring
-        adapter['image'] = image_url
-
         specifications = {}
         specs = adapter.get('specifications')
         if specs:
